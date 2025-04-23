e70cff684aa1526d9b8b246b6f0d6f1220e0657a
Pedro Guidotte <peguidotte@gmail.com>
2025-04-23 14:36:39 -0300

    Fix: Entidades fracas de Products

--- src/product/product.entity.ts
+++ src/product/product.entity.ts
@@ -1,21 +1,44 @@
-class ProductFeature {
-    name: string;
-    description: string;
-}
+import { Column, Entity, UpdateDateColumn, PrimaryGeneratedColumn, DeleteDateColumn, CreateDateColumn } from "typeorm";
+import { ProductFeatureEntity } from "./product-feature.entity";
+import { ProductImageEntity } from "./product-image.entity";
 
-class ProductImage {
-    url: string;
-    description: string;
-}
 
+@Entity({ name: 'products' })
 export class ProductEntity {
+
+    @PrimaryGeneratedColumn('uuid', { name: 'id' })
     id: string;
+
+    @Column({ name: 'user_id', length: 100, nullable: false })
     userId: string;
+
+    @Column({ name: 'name', length: 100, nullable: false })
     name: string;
+
+    @Column({ name: 'price', type: 'decimal', precision: 10, scale: 2, nullable: false })
     price: number;
+
+    @Column({ name: 'quantity', type: 'int', nullable: false })
     quantity: number;
+
+    @Column({ name: 'description', type: 'text', nullable: false })
     description: string;
+
+    @Column({ name: 'category', length: 50, nullable: false })
     category: string;
-    features: ProductFeature[];
-    images: ProductImage[];
+    
+    
+    features: ProductFeatureEntity[];
+
+    images: ProductImageEntity[];
+
+    @CreateDateColumn({ name: 'created_at', type: 'timestamp' })
+    createdAt: string;
+
+    @UpdateDateColumn({ name: 'updated_at', type: 'timestamp', nullable: true })
+    updatedAt: string;
+
+    @DeleteDateColumn({ name: 'deleted_at', type: 'timestamp', nullable: true })
+    deletedAt: string;
+
 }
--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -1,9 +1,27 @@
+import { Entity, Column, CreateDateColumn, UpdateDateColumn, DeleteDateColumn, PrimaryColumn, PrimaryGeneratedColumn} from 'typeorm'
 
+@Entity({name: 'users'})
 export class UserEntity {
 
+    @PrimaryGeneratedColumn('uuid', {name: 'id'})
     id: string;
+
+    @Column({name: 'name', length: 100, nullable: false})
     name: string;
+
+    @Column({name: 'email', length: 70, unique: true, nullable: false})
     email: string;
+
+    @Column({name: 'password', length: 255, nullable: false})
     password: string;
+
+    @CreateDateColumn({name: 'created_at', type: 'timestamp'})
+    createdAt: string;
+
+    @UpdateDateColumn({name: 'updated_at', type: 'timestamp', nullable: true})
+    updatedAt: string;
+
+    @DeleteDateColumn({name: 'deleted_at', type: 'timestamp', nullable: true})
+    deletedAt: string;
 
 }
